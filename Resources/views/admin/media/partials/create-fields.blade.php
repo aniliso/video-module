250562ee6b5a372c7e26451def6975cb115c04e4
sagsoz06 <designer@visualturk.net>
2019-01-24 00:44:49 +0300

    Fixed localization

--- Resources/views/admin/media/partials/create-fields.blade.php
+++ Resources/views/admin/media/partials/create-fields.blade.php
@@ -1,7 +1,7 @@
 <div class="box-body">
-    {!! Form::i18nInput('title', trans('video::categories.form.title'), $errors, $lang, null, ['data-slug' => 'source', 'v-model'=>'video.title']) !!}
+    {!! Form::i18nInput('title', trans('video::categories.form.title'), $errors, $lang, null, ['data-slug' => 'source', 'class'=>'form-control title']) !!}
 
-    {!! Form::i18nInput('slug', trans('video::categories.form.slug'), $errors, $lang, null, ['data-slug' => 'target', 'v-model'=>'video.slug']) !!}
+    {!! Form::i18nInput('slug', trans('video::categories.form.slug'), $errors, $lang, null, ['data-slug' => 'target', 'class'=>'form-control slug']) !!}
 
-    {!! Form::i18nTextarea('description', trans('video::categories.form.description'), $errors, $lang, null, ['class'=>'form-control textarea', ':value.prop'=>'video.description']) !!}
+    {!! Form::i18nTextarea('description', trans('video::categories.form.description'), $errors, $lang, null, ['class'=>'form-control textarea']) !!}
 </div>
--- Resources/views/admin/media/partials/url-field.blade.php
+++ Resources/views/admin/media/partials/url-field.blade.php
@@ -1,5 +1,8 @@
 <div class="box-body">
     <div class="input-group input-group-sm" v-bind:class="video.active ? 'has-success' : 'has-error'">
+      <span class="input-group-addon">
+        <input type="checkbox" aria-label="" v-model="update">
+      </span>
         {!! Form::input('text', 'video_link', old('video', $media->video_link ?? ''), ['class'=>'form-control','v-model'=>'video.video_link']) !!}
         <span class="input-group-btn">
             <button type="button" class="btn btn-success btn-flat" @click="getEmbed()"><i class="fa fa-download"></i> URL'den getir</button>
@@ -19,11 +22,8 @@
         new Vue({
             el: '#video-form',
             data: {
+                update:false,
                 video: {
-                    id: '{{ $media->id ?? '' }}',
-                    title: '{{ $media->title ?? '' }}',
-                    slug: '{{ $media->slug ?? '' }}',
-                    description: '{!! isset($media) ? htmlspecialchars($media->description, ENT_QUOTES) : '' !!}',
                     video_link: '{{ $media->video_link ?? '' }}',
                     active: {{ isset($media) ? $media->embed['code'] ? 'true' : 'false' : 'false' }},
                     code: '{!! $media->embed['code'] ?? '' !!}'
@@ -33,12 +33,12 @@
                 getEmbed() {
                     axios.get('{{ route('api.media.getEmbed') }}?url='+this.video.video_link)
                         .then(response=>{
-                            this.video.title       = response.data.data.title;
-                            this.video.slug        = response.data.data.slug;
-                            this.video.description = response.data.data.description;
                             this.video.code        = response.data.data.code;
                             this.video.active      = true;
-                            $('.wysihtml5-sandbox').contents().find('body').html(this.video.description);
+                            if(this.update) {
+                                $('.title').val(response.data.data.title);
+                                $('.wysihtml5-sandbox').contents().find('body').html(response.data.data.description);
+                            }
                         });
                 }
             }
